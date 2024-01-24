b2c84ec4f8533e26c7b6f72570195716773fdf22
Ryan Blackman <rblackman@mtd.org>
2024-01-24 14:34:19 -0600

    add shape methods

--- Repositories/Transit/IShapeRepository.cs
+++ Repositories/Transit/IShapeRepository.cs
@@ -6,6 +6,8 @@ namespace Mtd.Stopwatch.Core.Repositories.Transit
 	public interface IShapeRepository<T_Collection> : IAsyncReadable<Shape, T_Collection>, IAsyncWriteable<Shape, T_Collection>, IAsyncIdentifiable<string, Shape>, IDisposable
 		where T_Collection : IEnumerable<Shape>
 	{
-
+		public Task<T_Collection> GetAllWithShapePointsAsync(CancellationToken cancellationToken);
+		public Task<Shape> GetByIdentityWithShapePointsAsync(CancellationToken cancellationToken);
+		public Task<Shape?> GetByIdentityWithShapePointsOrDefaultAsync(CancellationToken cancellationToken);
 	}
 }
