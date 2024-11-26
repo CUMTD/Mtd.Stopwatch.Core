39ce7c06e1f99889534ae9c34023889918de8a7d
matt novelli <mattnovelli@gmail.com>
2024-11-26 10:38:01 -0600

    add direction repo interface (#12)

---
+++ Repositories/Schedule/IDirectionRepository.cs
@@ -0,0 +1,10 @@
+using Mtd.Core.Repositories;
+using Mtd.Stopwatch.Core.Entities.Schedule;
+
+namespace Mtd.Stopwatch.Core.Repositories.Schedule
+{
+	public interface IDirectionRepository<T_Collection> : IAsyncReadable<Direction, T_Collection>, IAsyncWriteable<Direction, T_Collection>, IAsyncIdentifiable<string, Direction>, IDisposable
+		where T_Collection : IEnumerable<Direction>
+	{
+	}
+}
