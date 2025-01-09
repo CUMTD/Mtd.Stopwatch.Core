a329401a72dd7fa0cb1240ee7d91f0fdf07ab850
Ryan Blackman <rblackman@mtd.org>
2025-01-09 15:09:15 -0600

    Update IPublicRouteGroupRepository.cs to have optional includes (#14)

--- Repositories/Schedule/IPublicRouteGroupRepository.cs
+++ Repositories/Schedule/IPublicRouteGroupRepository.cs
@@ -6,8 +6,8 @@ namespace Mtd.Stopwatch.Core.Repositories.Schedule
 	public interface IPublicRouteGroupRepository<T_Collection> : IAsyncReadable<PublicRouteGroup, T_Collection>, IAsyncWriteable<PublicRouteGroup, T_Collection>, IAsyncIdentifiable<string, PublicRouteGroup>, IDisposable
 		where T_Collection : IEnumerable<PublicRouteGroup>
 	{
-		Task<T_Collection> GetAllWithPublicRoutesAsync(CancellationToken cancellationToken);
+		Task<T_Collection> GetAllWithPublicRoutesAsync(CancellationToken cancellationToken, bool includeDirections = false, bool includeDaytypes = false);
 
-		Task<PublicRouteGroup> GetByIdentityWithPublicRoutesAsync(string identity, CancellationToken cancellationToken);
+		Task<PublicRouteGroup> GetByIdentityWithPublicRoutesAsync(string identity, CancellationToken cancellationToken, bool includeDirections = false, bool includeDaytype = false);
 	}
 }
