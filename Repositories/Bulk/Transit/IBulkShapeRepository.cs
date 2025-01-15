3dd941957d0fb6f9704a99ab55c490ca4c913ada
Ryan Blackman <rblackman@mtd.org>
2025-01-15 09:04:00 -0600

    Add async method to truncate shapes in IBulkShapeRepository (#20)
    A new method `TruncateShapesAndShapePointsAsync` has been added
    to the `IBulkShapeRepository` interface. This method is
    asynchronous and takes a `CancellationToken` as a parameter.
    It is intended to truncate shapes and shape points.

--- Repositories/Bulk/Transit/IBulkShapeRepository.cs
+++ Repositories/Bulk/Transit/IBulkShapeRepository.cs
@@ -7,4 +7,5 @@ namespace Mtd.Stopwatch.Core.Repositories.Bulk.Transit;
 public interface IBulkShapeRepository<T_Collection> : IShapeRepository<T_Collection>, IAsyncBulkWriteable<Shape, T_Collection>
 	where T_Collection : IEnumerable<Shape>
 {
+	Task TruncateShapesAndShapePointsAsync(CancellationToken cancellationToken);
 }
