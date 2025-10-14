f21786f763dcc6d4be0fa391f7264e0645722c21
Matthew Novelli <mattnovelli@gmail.com>
2025-10-14 15:10:21 -0500

    add IShapePointRepository and IBulkShapePointRepository

---
+++ Repositories/Bulk/Transit/IBulkShapePointRepository.cs
@@ -0,0 +1,10 @@
+﻿using Mtd.Core.Repositories;
+using Mtd.Stopwatch.Core.Entities.Transit;
+using Mtd.Stopwatch.Core.Repositories.Transit;
+
+namespace Mtd.Stopwatch.Core.Repositories.Bulk.Transit;
+
+public interface IBulkShapePointRepository<T_Collection> : IShapeRepository<T_Collection>, IAsyncBulkWriteable<Shape, T_Collection>
+	where T_Collection : IEnumerable<Shape>
+{
+}
---
+++ Repositories/Transit/IShapePointRepository.cs
@@ -0,0 +1,10 @@
+﻿using Mtd.Core.Repositories;
+using Mtd.Stopwatch.Core.Entities.Transit;
+
+namespace Mtd.Stopwatch.Core.Repositories.Transit;
+
+public interface IShapePointRepository<T_Collection> : IAsyncReadable<ShapePoint, T_Collection>, IAsyncWriteable<ShapePoint, T_Collection>, IDisposable
+	where T_Collection : IEnumerable<ShapePoint>
+{
+
+}
